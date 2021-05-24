708d1845dc8fa8e5b92a90e2afb97fc034bc0a4f
zenzx <2362315840@qq.com>
2021-05-24 10:17:23 +0800

    配置路由

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room')
 ]
