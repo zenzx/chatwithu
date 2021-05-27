6bde495dd76eb2c1077c5910fd789d8f9acbef7e
zenzx <2362315840@qq.com>
2021-05-27 16:57:51 +0800

    #bug 因为模块导入的顺序不同导致daphne不能用

--- chatwithu/asgi.py
+++ chatwithu/asgi.py
@@ -9,16 +9,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+import chat.routing
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatwithu.settings")
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatwithu.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
